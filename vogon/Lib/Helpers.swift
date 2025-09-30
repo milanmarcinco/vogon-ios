4d63a0eddbefb28b87eb0e6a0e8942503513ff7e
Milan Marcinčo <mmsprava@gmail.com>
2025-09-30 16:45:45 +0200

    Minor fix

--- vogon/Lib/Helpers.swift
+++ vogon/Lib/Helpers.swift
@@ -2,9 +2,11 @@ import Foundation
 import CoreBluetooth
 
 func getCharacteristicValue(_ characteristic: CBCharacteristic, as type: CharacteristicValueType) -> String {
-	var value = "N/A"
+	var value = ""
 	
-	if let data = characteristic.value {
+    if let data = characteristic.value {
+        if data.isEmpty { return value }
+        
 		switch type {
 			case .number:
 				let number = data.withUnsafeBytes { bytes in bytes.load(as: UInt16.self) }
